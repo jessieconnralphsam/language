7aa13a702e892bf83b35be1798a190a115564c07
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-03-10 09:38:32 +0800

    final

--- includes/login.php
+++ includes/login.php
@@ -13,23 +13,21 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get username and password from the form
+
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Prepare SQL statement to retrieve user data
     $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) == 1) {
-        // User found, set session variables and redirect to another page
+
         $_SESSION['username'] = $username;
-        header("Location: ../manage.php"); // Change to your desired page
+        header("Location: ../manage.php");
         exit();
     } else {
-        header("Location: ../login.php?invalid=true"); // Add a parameter indicating invalid login
+        header("Location: ../login.php?invalid=true"); 
         exit();
     }
 }
